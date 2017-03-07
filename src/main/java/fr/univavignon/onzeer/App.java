55f2a50f3daa3de316df241d0608ea9bc56adbcb
allonzo <simviv28@hotmail.fr>
2017-03-07 16:33:20 +0100

    1er commit

---
+++ src/main/java/fr/univavignon/onzeer/App.java
@@ -0,0 +1,13 @@
+package fr.univavignon.onzeer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
