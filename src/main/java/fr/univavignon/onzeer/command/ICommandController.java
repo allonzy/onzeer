a47025152d5dd4a199d49e0f8aa8457295b1e376
allonzo <simviv28@hotmail.fr>
2017-03-09 08:38:18 +0100

    why am i commiting

--- src/main/java/fr/univavignon/onzeer/command/ICommandController.java
+++ src/main/java/fr/univavignon/onzeer/command/ICommandController.java
@@ -4,5 +4,10 @@ import fr.univavignon.onzeer.exceptions.*;
 import fr.univavignon.onzeer.model.*;
 
 public interface ICommandController {
+	/**
+	 * 
+	 * @param audioFile
+	 * @throws FileNotFoundException
+	 */
 	public void startAudioStreamingOf(AudioFileMetadata audioFile) throws FileNotFoundException;
 }
